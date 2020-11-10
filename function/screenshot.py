a03ef98b4cabbbb8b445b8ca90b3f68a7ca56a3c
yinzhixiang <yinzhixiang@ygsoft.com>
2020-11-10 17:03:03 +0800

    修改截屏py文件

--- function/screenshot.py
+++ function/screenshot.py
@@ -4,11 +4,11 @@ from selenium import webdriver     #从selenium库导入webdirver
 
 def screenshot(save_path):
     global driver
-    chrome_path = "C:/Users/zhengtianrui/Desktop/chromedriver_win32/chromedriver.exe"
+    chrome_path = "E:/chormedriver/74.0.3729.6/chromedriver.exe"
     os.environ['webdriver.chrome.driver'] = chrome_path
     driver = webdriver.Chrome(chrome_path)
     sleep(1)
-    driver.get("file:///C:/Program Files (x86)/Jenkins\workspace/Auto_Test_Api/Test_Ecs_Interface/reports/SummaryReport.html")
+    driver.get("file:///C:/Windows/System32/config/systemprofile/AppData/Local/Jenkins/.jenkins/workspace/ygjy_parkpass/default/reports/SummaryReport.html")
     driver.maximize_window()
     driver.implicitly_wait(10)
     driver.save_screenshot(save_path)
